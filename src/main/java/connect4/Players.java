05ecf33e3c3bd322018c3de9ea54543f5fdeae86
EsraKa <esra.karakus@ynov.com>
2016-12-01 15:03:17 +0100

    update update

--- src/main/java/connect4/Players.java
+++ src/main/java/connect4/Players.java
@@ -1,7 +1,27 @@
 package connect4;
+import java.util.Scanner;
 
 /**
  * Created by Kakashi on 24/11/2016.
  */
 public class Players {
+
+  protected static Scanner scanner = new Scanner(System.in);
+
+
+  void CreatePlayer()
+  {
+    for (int i=1; i<3;i++) {
+
+      System.out.println("Player" + i + " \nEnter your name: ");
+      String NamePlayer1 = scanner.nextLine();
+      System.out.println("--");
+      System.out.println("You entered: " + NamePlayer1);
+      System.out.println("\n");
+
+
+    }
+
+
+  }
 }
