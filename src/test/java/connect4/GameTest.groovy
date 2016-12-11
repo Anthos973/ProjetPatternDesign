b63420a743e62f0f8d255b6f5c44839405abf628
EsraKa <esra.karakus@ynov.com>
2016-12-11 21:27:01 +0100

    diagram

---
+++ src/test/java/connect4/GameTest.groovy
@@ -0,0 +1,19 @@
+package connect4
+
+/**
+ * Created by Esra on 11/12/2016.
+ */
+class GameTest {
+  void setUp() {
+    super.setUp()
+
+  }
+
+  void tearDown() {
+
+  }
+
+  void testGetBoard() {
+
+  }
+}
---
+++ src/test/java/connect4/StateTest.groovy
@@ -0,0 +1,15 @@
+package connect4
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Esra on 11/12/2016.
+ */
+class StateTest {
+
+  @Test
+  void testGetBoard() {
+    
+  }
+}
